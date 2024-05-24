3da5794d57a2c0d044832d22dbdd1fad5da29e27
mohamedragab33 <moha.ragab33@gmail.com>
2024-05-25 01:24:51 +0300

    Add Circuit Breaker

--- Demo App/MicroService1/src/main/java/com/msa/microservice1/MicroService1Application.java
+++ Demo App/MicroService1/src/main/java/com/msa/microservice1/MicroService1Application.java
@@ -1,23 +1,32 @@
 package com.msa.microservice1;
 
+import com.netflix.hystrix.contrib.javanica.annotation.HystrixCommand;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.circuitbreaker.EnableCircuitBreaker;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@EnableCircuitBreaker
 public class MicroService1Application {
 
     public static void main(String[] args) {
         SpringApplication.run(MicroService1Application.class, args);
     }
 @RestController
-    class MicroServiceController{
+static
+class MicroServiceController{
 
         @GetMapping("api/msa1/message")
+        @HystrixCommand(fallbackMethod = "displayDefaultMessage")
         public String displayMessage() {
             return "Hello from MicroService1";
         }
+    public String displayDefaultMessage() {
+        return "Hello from Default message from  MicroService1";
+    }
+
 
     }
 }
